034f85c37004d3ade9a68f6123624e1887dc3140
mehmetayesilkaya <mehmeta.yesilkaya@gmail.com>
2026-02-14 19:20:55 +0300

    feat(reverseString): implement solution and pass all tests

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (reversed) {
+  return reversed.split('').reverse().join('');
 };
 
+console.log(reverseString('JavaScript'));
 // Do not edit below this line
 module.exports = reverseString;
