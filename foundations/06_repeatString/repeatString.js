58598313999b71bf19d912b429f5af22d297a0a2
mehmetayesilkaya <mehmeta.yesilkaya@gmail.com>
2026-02-14 19:00:03 +0300

    feat(repeatString): implement solution and pass all tests

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let value = '';
+  if (num < 0) return 'ERROR';
 
+  for (let i = 0; i < num; i++) {
+    value += string;
+  }
+  return value;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
