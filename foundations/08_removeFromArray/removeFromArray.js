57d735527b8d8129a62bfe3d0a83d51728cfde5d
mehmetayesilkaya <mehmeta.yesilkaya@gmail.com>
2026-02-15 04:40:52 +0300

    feat: solve removeFromArray and pass all tests

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...argsToRemove) {
+  return arr.filter((item) => !argsToRemove.includes(item));
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
