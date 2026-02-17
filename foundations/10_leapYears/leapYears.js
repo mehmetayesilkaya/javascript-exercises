984ed1804ac4c78c89752c0ca9ee0aef93e4823f
mehmetayesilkaya <mehmeta.yesilkaya@gmail.com>
2026-02-17 04:13:26 +0300

    feat(leapYears): implement solution and pass all tests

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
+const leapYears = (year) =>
+  (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 
-};
-
+console.log(leapYears(2000)); // is a leap year: returns true
+console.log(leapYears(1985)); // is not a leap year: returns false
+console.log(leapYears(2100));
 // Do not edit below this line
 module.exports = leapYears;
