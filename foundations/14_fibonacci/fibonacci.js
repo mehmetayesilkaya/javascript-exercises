9a28334bb554de767ee94d8665a8348d224b7759
mehmetayesilkaya <mehmeta.yesilkaya@gmail.com>
2026-09-07 13:30:35 +0300

    Fix: fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (nthFib) {
+  if (Number(nthFib) === 1 || Number(nthFib) == 2) return 1;
+  if (Number(nthFib) < 0) return "OOPS";
+  if (Number(nthFib) == 0) return 0;
+  let prev2 = 1;
+  let prev1 = 1;
+  let result = 1;
+  for (let n = 3; n <= nthFib; n++) {
+    result = prev1 + prev2;
+    prev2 = prev1;
+    prev1 = result;
+  }
+  return result;
 };
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
