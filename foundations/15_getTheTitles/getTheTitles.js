752715831b6c7a736298e442d7e305aee108461f
mehmetayesilkaya <mehmeta.yesilkaya@gmail.com>
2026-09-07 13:53:32 +0300

    fix: getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (getTitles) {
+  const titles = getTitles.map((book) => book.title);
+  return titles;
 };
 
 // Do not edit below this line
