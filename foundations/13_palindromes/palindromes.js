632d8d70daf9a8dce3c134e19af2ebd16459ee5e
mehmetayesilkaya <mehmeta.yesilkaya@gmail.com>
2026-09-07 11:15:51 +0300

    Fix: palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (pal) {
+  const validCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleanCh = pal
+    .toLowerCase()
+    .split("")
+    .filter((char) => validCharacters.includes(char))
+    .join("");
+
+  const reversed = cleanCh.split("").reverse().join("");
+
+  return reversed == cleanCh;
 };
+
+console.log(palindromes("A man, a plan, a canal: Panama!"));
 
 // Do not edit below this line
 module.exports = palindromes;
